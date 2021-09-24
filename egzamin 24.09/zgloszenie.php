9acb1dc1c47c92a6d002e84914df425540a96bc4
kabukuba1234 <72551199+kabukuba1234@users.noreply.github.com>
2021-09-24 12:15:40 +0200

    Add files via upload

---
+++ egzamin 24.09/zgloszenie.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wedkowanie - moje hobby</title>
+</head>
+
+<body>
+    <?php 
+        $lowisko = $_POST['lowisko'];
+        $data = $_POST['data'];
+        $sedzia = $_POST['sedzia'];
+
+        $db = new mysqli('localhost', 'root','','wedkarstwo');
+        $sql = "INSERT INTO `zawody_wedkarskie` (`id`,`karty_wedkarskie_id`,`lowisko_id`,`data_zawodow`,`sedzia`)
+                 VALUES (NULL, '2','$lowisko', '$data','$sedzia')";
+        $db->query($sql);
+        $db->close();
+    ?>
+</body>
+
+</html>
